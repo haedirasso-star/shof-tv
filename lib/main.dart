6444c16e0c400be6ba2728a40fa99e551e62f8a9
haedirasso-star <haedirasso@gmail.com>
2026-01-31 13:21:47 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+void main() {
+  // تفعيل واجهة Shof TV الأولى
+  runApp(const ShofTVApp());
+}
+
+class ShofTVApp extends StatelessWidget {
+  const ShofTVApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shof TV',
+      debugShowCheckedModeBanner: false,
+      // السمة السوداء والذهبية التي طلبتها
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.yellowAccent,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          elevation: 0,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
