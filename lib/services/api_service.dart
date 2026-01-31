511edb3455a35d2b5255c8f4ca98fb96882210a7
haedirasso-star <haedirasso@gmail.com>
2026-01-31 13:16:09 +0300

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,1 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+class ApiService {
+  final String apiKey = "5b166a24c91f59178e8ce30f1f3735c0"; // مفتاح Shof TV الخاص بك
+  final String baseUrl = "https://api.themoviedb.org/3";
+
+  // جلب الأفلام والمسلسلات العربية والتركية المدبلجة
+  Future<List<dynamic>> fetchShofContent(String type) async {
+    final response = await http.get(Uri.parse(
+        "$baseUrl/discover/$type?api_key=$apiKey&with_original_language=ar|tr&sort_by=popularity.desc"));
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body)['results'];
+    } else {
+      throw Exception('فشل جلب البيانات من السيرفر');
+    }
+  }
+}
