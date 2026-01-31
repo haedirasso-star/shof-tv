ad8ddcec537dce27b8c73d656bac0853cf2edb63
haedirasso-star <haedirasso@gmail.com>
2026-01-31 20:08:42 +0300

    Update player_screen.dart

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -22,10 +22,12 @@ class _PlayerScreenState extends State<PlayerScreen> {
   @override
   void initState() {
     super.initState();
+    // فحص الرابط: إذا لم يكن فيديو مباشر (m3u8) نفتحه كمتصفح للأفلام
     if (!widget.videoUrl.contains('.m3u8') && !widget.videoUrl.contains('.mp4')) {
       _isWebView = true;
       _webViewController = WebViewController()
         ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setBackgroundColor(Colors.black)
         ..loadRequest(Uri.parse(widget.videoUrl));
       setState(() { _isInitialized = true; });
     } else {
@@ -34,23 +36,41 @@ class _PlayerScreenState extends State<PlayerScreen> {
   }
 
   void _initVideo() async {
-    _videoController = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl));
-    await _videoController!.initialize();
-    _chewieController = ChewieController(videoPlayerController: _videoController!, autoPlay: true, isLive: true);
-    setState(() { _isInitialized = true; });
+    try {
+      _videoController = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl));
+      await _videoController!.initialize();
+      _chewieController = ChewieController(
+        videoPlayerController: _videoController!,
+        autoPlay: true,
+        isLive: true,
+        aspectRatio: _videoController!.value.aspectRatio,
+      );
+      setState(() { _isInitialized = true; });
+    } catch (e) {
+      debugPrint("خطأ في المشغل: $e");
+    }
   }
 
   @override
-  void dispose() { _videoController?.dispose(); _chewieController?.dispose(); super.dispose(); }
+  void dispose() {
+    _videoController?.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(title: Text(widget.title ?? "Shof TV"), backgroundColor: Colors.black),
+      appBar: AppBar(
+        title: Text(widget.title ?? "Shof TV", style: const TextStyle(color: Colors.yellow)),
+        backgroundColor: Colors.black,
+      ),
       body: !_isInitialized 
         ? const Center(child: CircularProgressIndicator(color: Colors.yellow))
-        : _isWebView ? WebViewWidget(controller: _webViewController!) : Chewie(controller: _chewieController!),
+        : _isWebView 
+            ? WebViewWidget(controller: _webViewController!) 
+            : Chewie(controller: _chewieController!),
     );
   }
 }
