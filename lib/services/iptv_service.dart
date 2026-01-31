bfa1822cc32689294ce6908545dc583e6fb0a639
haedirasso-star <haedirasso@gmail.com>
2026-01-31 13:23:20 +0300

    Update iptv_service.dart

--- lib/services/iptv_service.dart
+++ lib/services/iptv_service.dart
@@ -1,1 +1,23 @@
+import 'package:http/http.dart' as http;
 
+class IptvService {
+  // جلب روابط القنوات المباشرة
+  Future<List<Map<String, String>>> fetchArabicChannels() async {
+    const String url = "https://raw.githubusercontent.com/iptv-org/iptv/master/groups/arabic.m3u";
+    final response = await http.get(Uri.parse(url));
+    
+    List<Map<String, String>> channels = [];
+    if (response.statusCode == 200) {
+      final lines = response.body.split('\n');
+      for (int i = 0; i < lines.length; i++) {
+        if (lines[i].startsWith('#EXTINF')) {
+          // استخراج الاسم والرابط
+          String name = lines[i].split(',').last;
+          String link = lines[i + 1];
+          channels.add({"name": name, "url": link});
+        }
+      }
+    }
+    return channels;
+  }
+}
